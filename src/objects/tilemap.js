8b20a2bd70af6eee3b397f3695235a15c4efa4bb
Michelle Dacal <michdacal@gmail.com>
2021-03-30 15:53:01 -0400

    feat(functions): cleaned up code

--- src/objects/tilemap.js
+++ src/objects/tilemap.js
@@ -171,8 +171,7 @@ const tilemap = [
     new Tile(2, 'Graduation Day!', pickLifeTile),
     new Tile(0, 'Choose a career.', pickCareer),
     new Tile(3, 'Payday!', payday),
-    // new Tile(2, 'Birthday Party!', pickLifeTile),
-    new Tile(3, 'Taxes?', taxesDue),
+    new Tile(2, 'Birthday Party!', pickLifeTile),
     new Tile(' '),
     new Tile(1, 'Pay $15,000.', (scene, amount) =>
       pay(scene, (amount = 15000))
