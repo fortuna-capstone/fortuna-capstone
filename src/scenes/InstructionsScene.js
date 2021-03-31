2c16c7849623f423b6152aa5ab3a57dc43cd509d
Hatibe Kabulantok <hatibekara@gmail.com>
2021-03-31 13:28:50 -0700

    Creates instructions scene and object

---
+++ src/scenes/InstructionsScene.js
@@ -0,0 +1,11 @@
+import 'phaser'
+
+this.instructions;
+
+export default class Instructions extends from Phaser.Scene {
+  constructor() {
+    super()
+    this.instructions = this.add.text('')
+  }
+
+}
