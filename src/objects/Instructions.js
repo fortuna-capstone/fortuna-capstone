57bd20a277030f59fc5cfba13410833029cadde3
Hatibe Kabulantok <hatibekara@gmail.com>
2021-04-01 08:09:38 -0700

    Saves work

--- src/objects/Instructions.js
+++ src/objects/Instructions.js
@@ -1,7 +1,18 @@
 import MessageBox from MessageBox
 
 export default class Instructions extends Phaser.GameObjects.Container {
-  constructor() {
+  constructor(scene,
+    x,
+    y,
+    key1,
+    key2,
+    key3,
+    description,
+    text1,
+    text2,
+    decision1,
+    decision2,
+    callback) {
     super()
   }
 
