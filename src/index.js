28dc8515ef872b94ecf763347b36500dff9bfafe
Hatibe Kabulantok <hatibekara@gmail.com>
2021-04-02 02:02:33 -0700

    Adds text to instructions scene

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import 'phaser';
 import phaserConfig from './config/phaserConfig';
 import GameScene from './scenes/GameScene';
-import WaitingScene from './scenes/WaitingScene'
+import WaitingScene from './scenes/WaitingScene';
 import TitleScene from './scenes/TitleScene';
 import InstructionsScene from './scenes/InstructionsScene';
-
 
 class Game extends Phaser.Game {
   constructor() {
     super(phaserConfig);
-    this.scene.add('InstructionsScene');
+    this.scene.add('InstructionsScene', InstructionsScene);
     this.scene.add('Title', TitleScene);
     this.scene.add('Game', GameScene);
-    this.scene.add('Waiting', WaitingScene)
-    this.scene.start('Title');
+    this.scene.add('Waiting', WaitingScene);
+    this.scene.start('InstructionsScene');
   }
 }
 
--- src/scenes/InstructionsScene.js
+++ src/scenes/InstructionsScene.js
@@ -10,14 +10,24 @@ export default class InstructionsScene extends Phaser.Scene {
   preload() {
     this.load.image('messageBox', 'assets/message_box.png');
     this.load.image('blueButton1', 'assets/blue_button02.png');
+    console.log('IS THIS WORKING');
   }
 
   create() {
-    // this.messageBox = new MessageBox(this, 0, 0, 'messageBox', 'blueButton1');
-    // this.scene.add.text(0, 0, 'WHAT"S HEREEEE', {
-    //   fontSize: '32px',
-    //   fill: '#fff',
-    // });
-    this.text = new MessageBox();
+    let scene = this;
+    // let welcome = this.add.image(400, 300, 'box');
+
+    let text = this.add
+      .text(0, 0, 'WELCOME TO GAME OF LIFE: DEVELOPER EDITION!', {
+        fontSize: '32px',
+        fill: '#fff',
+        wordWrap: { width: 220 },
+        align: 'center',
+      })
+      .setDepth(6);
+
+    Phaser.Display.Align.In.TopCenter(text);
+    text.y = 210;
+    console.log('IS THIS WORKING');
   }
 }
