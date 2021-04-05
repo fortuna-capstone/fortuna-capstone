259c3567761ce4850f74f2db4bd7aef7cd003e81
Ana Judd <anajudd2015@gmail.com>
2021-04-05 10:16:29 -0500

    fix(merge): Resolves merge

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,8 +2,7 @@ import 'phaser';
 
 import 'firebase/database';
 
-
-import Spinner from '../objects/Spinner'
+import Spinner from '../objects/Spinner';
 import phaserConfig from '../config/phaserConfig';
 
 import io from 'socket.io-client';
@@ -46,18 +45,23 @@ export default class GameScene extends Phaser.Scene {
     this.load.image('blueButton2', 'assets/blue_button03.png');
     this.load.image('messageBox', 'assets/message_box.png');
     this.load.image('otherPlayer', 'assets/grey_box.png');
-    this.load.image('backgroundImage', 'assets/gameScene.png')
-    this.load.image('playerOneBox', 'assets/playerOnePattern.png')
-    this.load.image('playerTwoBox', 'assets/playerTwoPattern.png')
-    this.load.image('playerThreeBox', 'assets/playerThreePattern.png')
-    this.load.spritesheet('spinner', 'assets/spinner.png', {frameWidth: 100, frameHeight: 100})
+    this.load.image('backgroundImage', 'assets/gameScene.png');
+    this.load.image('playerOneBox', 'assets/playerOnePattern.png');
+    this.load.image('playerTwoBox', 'assets/playerTwoPattern.png');
+    this.load.image('playerThreeBox', 'assets/playerThreePattern.png');
+    this.load.spritesheet('spinner', 'assets/spinner.png', {
+      frameWidth: 100,
+      frameHeight: 100,
+    });
   }
 
   create() {
     board = new MyBoard(this);
     let scene = this;
-    background = this.add.image(400, 300,'backgroundImage').setScale(4).setScrollFactor(0)
-    
+    background = this.add
+      .image(400, 300, 'backgroundImage')
+      .setScale(4)
+      .setScrollFactor(0);
 
     // CREATING BOARD
     this.board = new MyBoard(this);
@@ -407,20 +411,16 @@ export default class GameScene extends Phaser.Scene {
             (house) => action(this.scene, house)
           );
           this.socket.emit('endTurn');
-<<<<<<< HEAD
-        } else if ((tile.y === 0 && tile.x === 43) || 
-          (tile.y === 3 && (tile.x === 17 || tile.x === 39)) || 
+        } else if (
+          (tile.y === 0 && tile.x === 43) ||
+          (tile.y === 3 && (tile.x === 17 || tile.x === 39)) ||
           (tile.y === 4 && (tile.x === 24 || tile.x === 29 || tile.x === 35)) ||
-          (tile.y === 5 && tile.x === 20)) {
-          let turns = this.otherPlayers.getChildren().map(player => player.playerInfo.turn)
-          this.messageBox = new TradeBox(
-=======
-        } else if (tile.x === 8 && tile.y === 1) {
+          (tile.y === 5 && tile.x === 20)
+        ) {
           let turns = this.otherPlayers
             .getChildren()
             .map((player) => player.playerInfo.turn);
-          this.messageBox = new DecisionBox(
->>>>>>> main
+          this.messageBox = new TradeBox(
             this,
             camera.midPoint,
             0,
